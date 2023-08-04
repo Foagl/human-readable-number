93b776921ee8efc6d04f19392902a3ce5b5f2fb1
Foagl <ValeriySlykin@gmail.com>
2023-08-04 23:43:00 +0300

    feat: write function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function toReadable (number) {
-  
+    let str = number.toString();
+    let out = '';
+    // Missive of constant
+    const
+        o = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+        t = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighteen', 'ninety'],
+        h = ['one hundred', 'two hundred', 'three hundred ', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'],
+        p = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    
+
+    if(str.length == 1) 
+        return o[number-1];
+    else if(str.length == 2){
+        if(str[0] == 1)
+            out = p[parseInt(str[1])-1];
+        else 
+        out = (t[parseInt(str[0])-1] + ((str[1]!='0')?(' ' + o[parseInt(str[1])-1]):''));
+    }
+    else if(str.length == 3){
+        out = (h[parseInt(str[0])-1] + ((str[1]!='0')?(' ' + t[parseInt(str[1])-1]):'') + ((str[2]!='0')?(' ' + o[parseInt(str[2])-1]):''));
+    }
+    // make first letter High
+    let strOut = out.split('');
+    
+    strOut = strOut.join('');
+    return strOut;
 }
