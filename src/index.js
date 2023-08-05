e61959c9210d903f77265c567eab35cbb4c4b090
Foagl <ValeriySlykin@gmail.com>
2023-08-05 15:34:24 +0300

    fix: impruve function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 module.exports = function toReadable (number) {
+    if (number == 0)
+        return "zero";
+    
     let str = number.toString();
-    let out = '';
+    let out = '', i='';
     // Missive of constant
     const
         o = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
-        t = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighteen', 'ninety'],
-        h = ['one hundred', 'two hundred', 'three hundred ', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'],
+        t = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+        h = ['one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'],
         p = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    
 
     if(str.length == 1) 
         return o[number-1];
@@ -19,10 +21,13 @@ module.exports = function toReadable (number) {
     }
     else if(str.length == 3){
         out = (h[parseInt(str[0])-1] + ((str[1]!='0')?(' ' + t[parseInt(str[1])-1]):'') + ((str[2]!='0')?(' ' + o[parseInt(str[2])-1]):''));
+        i = Number(str[1] + str[2]);
+        if (i >= 10 && i <= 19 )
+        out+="teen";
+        out = out.replace(/ +/g, ' ').trim();
     }
-    // make first letter High
-    let strOut = out.split('');
     
+    let strOut = out.split('');
     strOut = strOut.join('');
     return strOut;
 }
